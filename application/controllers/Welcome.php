55b0cdb996fef66e3d91dfa8ed2d2d9cd29aac58
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2015-12-30 20:40:52 -0500

    - Stub MY_Output class for the MY_Output_Lib class from the   ci_tut_lib repo - Specifying the path to composer autoloader in the codeigniter's   autoload config file. - Welcome controller: Code that uses the new features of output.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper("string");
+		$this->output->nocache();
+	}
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -20,6 +26,15 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data = [
+			"date" => date("Y-m-d"),
+			"my_handle" => "jspreddy",
+			"my_name" => "sai"
+		];
+		$this->output->setDebug($data,"key");
+
+		$this->output->setDebug(["rand"=>random_string("alpha",36)], "rand");
+		
+		$this->output->sendOutput(200, []);
 	}
 }
---
+++ application/core/MY_Output.php
@@ -0,0 +1,8 @@
+<?php
+
+use CiLibs\MY_Output_Lib;
+
+class MY_Output extends MY_Output_Lib
+{
+
+}
